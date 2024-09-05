0ad2702a4085eb7fb53221c5365928771f061afc
adit5156 <adityasinghmpc2@gmail.com>
2024-09-05 21:10:56 +0530

    updated 05-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/04_conditional-statements/app.js
@@ -0,0 +1,22 @@
+// Conditional Statements
+
+// if-else
+
+// let age = 18;
+// if(age >= 18) {
+//     console.log("Yes! You can vote");
+//     console.log("Yes! You can drive");
+// }
+// else {
+//     console.log("Sorry! You can't vote!");
+// }
+
+
+// let firstName = "Aditya";
+
+// if(firstName == "Aditya") {
+//     console.log(`Welcome ${firstName} to the course.`);
+// }
+// else {
+//     console.log("User not found!!!");
+// }
---
+++ 13_JAVASCRIPT-LEVEL-2/04_conditional-statements/practice1.js
@@ -0,0 +1,11 @@
+let signalColor = "red";
+
+if(signalColor == "red") {
+    console.log("Stop");
+}
+else if(signalColor == "yellow") {
+    console.log("Get ready to go");
+}
+else if(signalColor == "green") {
+    console.log("Go");
+}

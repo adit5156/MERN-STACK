a107c0e9cd7cedf56f4169ebf7c3ea58a3faa40c
adit5156 <adityasinghmpc2@gmail.com>
2024-09-05 20:51:50 +0530

    updated 05-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/02_template-literals/app.js
@@ -0,0 +1,9 @@
+// Template Literals: they are used to embed expressions in the strings.
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+// let output = "The total sum is: " + (pencilPrice + eraserPrice) + " rupees";
+// let output = `The total sum is: ${pencilPrice + eraserPrice} rupees`;
+// console.log(output);
+
+console.log(`The total sum is: ${(pencilPrice + eraserPrice)} rupees`);

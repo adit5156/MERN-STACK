e0ec7ffdb1fe0c5411f1fed0a9fbc3742e943817
adit5156 <adityasinghmpc2@gmail.com>
2024-09-05 20:51:59 +0530

    updated 05-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/03_operators/app.js
@@ -0,0 +1,42 @@
+// let a = 10;
+// let b = 20;
+
+// Arithematic Operators
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+
+// Unary Operators
+
+// console.log(a++);   // 10
+// console.log(++a);   // 12
+
+// console.log(b--);   // 5
+// console.log(--b);   // 3
+
+
+// Assignment Operators
+
+// b = a;
+// b += a;  // 30
+// b -= a;  // 10
+// b *= a;  // 200
+// b /= a;  // 2
+// b %= a;  // 0
+// b **= a; // 10240000000000
+// console.log(b);
+
+
+// Comparsion Operator
+
+// let age = 18;
+// age = 16;
+
+// console.log(age > 18);
+// console.log(age >= 18);
+// console.log(age < 18);

3c9ee877f32495e31944ebecba78e6bc717b22e3
adit5156 <adityasinghmpc2@gmail.com>
2024-09-06 06:52:04 +0530

    updated 06-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/05_logical-operators/practice1.js
@@ -0,0 +1,8 @@
+let str = "akira";
+
+if(str[0] === "a" && str.length > 3) {
+    console.log("Good string");
+}
+else {
+    console.log("Not a good string");
+}
---
+++ 13_JAVASCRIPT-LEVEL-2/05_logical-operators/practice2.js
@@ -0,0 +1,8 @@
+let num = 12;
+
+if((num%3 === 0) && ((num + 1 === 15 || num - 1 === 11))) {
+    console.log("Safe");    // Safe
+}
+else {
+    console.log("Unsafe");
+}

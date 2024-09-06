550cc2116f8e76cbfd847f9209dfc7a92cd7f4d4
adit5156 <adityasinghmpc2@gmail.com>
2024-09-06 06:51:46 +0530

    updated 06-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/04_conditional-statements/nested-if-else.js
@@ -0,0 +1,14 @@
+let marks = 70;
+
+if(marks >= 33) {
+    console.log("Pass");
+    if(marks >= 80) {
+        console.log("Your grade is: Outstanding");
+    }
+    else {
+        console.log("Your grade is: A");
+    }
+}
+else {
+    console.log("Fail");
+}

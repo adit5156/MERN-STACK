31447f6a5106b7fb37c74b1806a589fffb20faae
adit5156 <adityasinghmpc2@gmail.com>
2024-09-06 06:52:26 +0530

    updated 06-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/06_switch-statement/switch.js
@@ -0,0 +1,15 @@
+let color = "red";
+
+switch(color) {
+    case "red": 
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Get ready to go");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+    default: 
+        console.log("Traffic Light Broken");
+}

dfa4bb51aea87500bf5bdc6db2e195dc08892d7a
adit5156 <adityasinghmpc2@gmail.com>
2024-09-06 06:52:36 +0530

    updated 06-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/07_alerts-and-prompts/app.js
@@ -0,0 +1,13 @@
+// console.log("Yeh hai ek message hai");
+// console.error("Yeh hai ek error message");
+// console.warn("Yeh ek warning message hai");
+
+// alert("Kuch toh gadbad hai Daya!!");
+
+// let name = prompt("Enter your name");
+// alert(`Welcome to my webpage, ${name}`);
+
+let firstName = prompt("Enter your first name");
+let lastName = prompt("Enter your last name");
+let fullName = firstName + " " + lastName;
+alert(`Welcome to my webpage, ${fullName}`);

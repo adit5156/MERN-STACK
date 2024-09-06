501c6fb84e2cb8fd66ba9990937cf609c7645ffc
adit5156 <adityasinghmpc2@gmail.com>
2024-09-06 06:52:46 +0530

    updated 06-09-2024

---
+++ 13_JAVASCRIPT-LEVEL-2/Assignment/app.js
@@ -0,0 +1,67 @@
+// Q1
+
+// let num = 5;
+// if(num % 10 === 0) {
+//     console.log("good");
+// }
+// else {
+//     console.log("bad");
+// }
+
+
+// Q2
+
+// let name = prompt("Enter your fullname");
+// let age = prompt("Enter your age");
+// alert(`${name} is ${age} years old`);
+
+// Q3
+
+// let quarterNumber = 1;
+// switch(quarterNumber) {
+//     case 1: 
+//         console.log("January, February, March");
+//         break;
+//     case 2: 
+//         console.log("April, May, June");
+//         break;
+//     case 3: 
+//         console.log("July, August, September");
+//         break;
+//     case 4:
+//         console.log("October, November, December");
+//         break;
+//     default: 
+//         console.log("Enter Valid Quarter Number");
+// }
+
+
+// Q4
+
+// let str = "aditya";
+// if((str[0] === 'A' || str[0] === 'a') && (str.length > 5)) {
+//     console.log("str is golden");
+// }
+// else {
+//     console.log("str is not golden");
+// }
+
+// Q5
+
+// let num1 = 2;
+// let num2 = 4;
+// let num3 = 6;
+
+// let largestNumber = Math.max(num1, num2, num3);
+// console.log("The largest number is: ", largestNumber);
+
+// Q6
+
+// let num1 = 32;
+// let num2 = 47852;
+// if(num1[num1.length - 1] === num2[num2.length - 1]) {
+//     console.log("The 2 numbers have the same last digit");
+// }
+// else {
+//     console.log("The 2 numbers does not have the same last digit");
+// }

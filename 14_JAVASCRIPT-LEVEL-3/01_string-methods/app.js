ddfc18334c7e4ea6d2b1fdda08b4bcfbfce7db70
adit5156 <adityasinghmpc2@gmail.com>
2024-09-07 13:59:22 +0530

    updated 07-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/01_string-methods/app.js
@@ -0,0 +1,51 @@
+// String methods - actions that can be performed on the objects
+
+// let msg = "         Hello           ";
+// console.log(msg.trim());
+
+// Trim Method
+
+// let password = prompt("Enter password");
+// let trimmedPassword = password.trim();
+// console.log(trimmedPassword);
+
+// toupperCase and toLowerCase Method
+
+// let name = "Aditya Singh";
+// console.log(name);
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+
+
+
+// indexOf Method
+
+// let msg = "ILoveCoding";
+// console.log(msg.indexOf('Love'));
+
+
+// Slice Method
+
+// let msg = "ILoveCoding";
+// console.log(msg.slice(0, 5));
+
+// let msg = "apnacollege";
+// console.log(msg.slice(4));
+// console.log(msg.slice(-1))  // msg.length - 1 => 11 - 1 = 10
+// console.log(msg.slice(-2))  // msg.length - 2 => 11 - 2 = 9
+// console.log(msg.slice(-3))  // msg.length - 3 => 11 - 3 = 8
+
+
+// Replace Method
+
+// let str = "ILoveCoding";
+// let newStr = str.replace('o', 'x');
+// let newStr = str.replace("Love", "Do");
+// console.log(newStr);
+
+
+// Repeat Method
+
+let str = "ILoveCoding";
+console.log(str);
+console.log(str.repeat(3));

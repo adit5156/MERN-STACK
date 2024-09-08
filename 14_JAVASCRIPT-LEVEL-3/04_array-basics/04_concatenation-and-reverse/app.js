f31741df2addbb92e1474642e08ccd49c3e427e9
adit5156 <adityasinghmpc2@gmail.com>
2024-09-08 10:10:36 +0530

    updated 08-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/04_array-basics/04_concatenation-and-reverse/app.js
@@ -0,0 +1,11 @@
+let primaryColors = ["red", "yellow", "blue"];
+let secondaryColors = ["orange", "green", "violet"];
+console.log("Primary Colors: ",primaryColors);
+console.log("Secondary Colors: ",secondaryColors);
+
+let allColors = primaryColors.concat(secondaryColors);
+console.log("All Colors: ",allColors);
+
+allColors.reverse();
+console.log("Reversed Colors: ", allColors);
+console.log(allColors);

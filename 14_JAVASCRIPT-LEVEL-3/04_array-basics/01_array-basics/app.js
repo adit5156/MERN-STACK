c5201fbd788a0f5fcfc745768179ae2c6128e5ad
adit5156 <adityasinghmpc2@gmail.com>
2024-09-08 10:09:16 +0530

    updated 08-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/04_array-basics/01_array-basics/app.js
@@ -0,0 +1,36 @@
+let students = ["aman", "shradha", "rajat"];
+console.log(students);
+
+let info = ["aman", 25, 6.1, true, undefined];
+console.log(info);
+
+let newArray = [];
+newArray.push("XII");
+newArray.push("Aditya");
+newArray.push(20);
+console.log(newArray);
+
+let fruits = ["mango", "apple", "litchi"];
+fruits[0] = "banana";
+fruits[1] = "pineapple";
+console.log(fruits);
+
+let cars = ["audi", "bmw", "xuv", "lamborghini"];
+console.log(cars);
+
+cars.push("toyota");
+cars.push("ferrari");
+console.log(cars);
+
+cars.pop();
+console.log(cars);
+
+cars.pop();
+console.log(cars);
+
+cars.unshift("mercedes");
+cars.unshift("rolls royce");
+console.log(cars);
+
+cars.shift();
+console.log(cars);

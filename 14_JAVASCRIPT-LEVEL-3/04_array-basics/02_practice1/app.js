d6ab2581923796fe7d7a3f258a407a98b427cea6
adit5156 <adityasinghmpc2@gmail.com>
2024-09-08 10:09:30 +0530

    updated 08-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/04_array-basics/02_practice1/app.js
@@ -0,0 +1,14 @@
+let months = ['january', 'july', 'march', 'august'];
+console.log(months);
+
+months.shift();
+console.log(months);
+
+months.shift();
+console.log(months);
+
+months.unshift('june');
+console.log(months);
+
+months.unshift('july');
+console.log(months);

37729322851ebe2c116057b4e35df78670019abc
adit5156 <adityasinghmpc2@gmail.com>
2024-09-08 10:10:21 +0530

    updated 08-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/04_array-basics/03_indexOf-and-includes-method/app.js
@@ -0,0 +1,9 @@
+let primary = ['red', 'yellow', 'blue'];
+
+console.log(primary.indexOf('red'));
+console.log(primary.indexOf('blue'));
+console.log(primary.indexOf('yellow'));
+console.log(primary.indexOf('Blue'));
+
+console.log(primary.includes('red'));
+console.log(primary.includes('Red'));

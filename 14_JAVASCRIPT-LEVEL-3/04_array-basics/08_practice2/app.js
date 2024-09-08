da86c264ef36d3c936f03fd93a6972ef1e4bf1d2
adit5156 <adityasinghmpc2@gmail.com>
2024-09-08 10:11:15 +0530

    updated 08-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/04_array-basics/08_practice2/app.js
@@ -0,0 +1,8 @@
+let months = ["january", "july", "march", "august"];
+months.splice(0, 2, "july", "june");
+console.log(months);
+
+let languages = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
+languages.reverse();
+console.log(languages);
+console.log(languages.indexOf("javascript"));

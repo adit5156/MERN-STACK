18b0989a1788fe6f94b5c27ff3c26ec6afb46d52
adit5156 <adityasinghmpc2@gmail.com>
2024-09-08 10:10:45 +0530

    updated 08-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/04_array-basics/05_slice/app.js
@@ -0,0 +1,8 @@
+// Slice -> copies a portion of an array
+let colors = ["red", "yellow", "blue", "orange", "pink", "white"];
+
+console.log(colors.slice());
+console.log(colors.slice(2));
+console.log(colors.slice(2,3));
+console.log(colors.slice(-2));
+console.log(colors);

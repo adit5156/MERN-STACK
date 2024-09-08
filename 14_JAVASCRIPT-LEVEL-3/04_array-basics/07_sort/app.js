62e179eb806190c5fcce9479b1ff72a233d2e66c
adit5156 <adityasinghmpc2@gmail.com>
2024-09-08 10:11:06 +0530

    updated 08-09-2024

---
+++ 14_JAVASCRIPT-LEVEL-3/04_array-basics/07_sort/app.js
@@ -0,0 +1,11 @@
+let cars = ["toyota", "gwagon", "xuv", "maruti", "bmw", "ferrari"];
+cars.sort();
+console.log(cars);
+
+let chars = ["b", "d", "c", "a", "e"];
+chars.sort();
+console.log(chars);
+
+let marks = [99, 89, 67, 42, 100];
+marks.sort();
+console.log(marks);

40dc4abf19fb56be8bfac5b458ee0daabf225a27
adit5156 <adityasinghmpc2@gmail.com>
2024-09-10 19:29:29 +0530

    updated 10-09-2024

--- 15_JAVASCRIPT-LEVEL-4/08_todo-app/app.js
+++ 15_JAVASCRIPT-LEVEL-4/08_todo-app/app.js
@@ -4,31 +4,25 @@ let req = prompt("Enter your request");
 
 while(true) {
     if(req === "quit") {
-        console.log("Quitting App");
+        console.log("Quitting App...");
         break;
-    };
-
+    }
     if(req === "list") {
-        console.log("---------------");
+        console.log("----------");
         for(let i = 0; i < todoTasks.length; i++) {
             console.log(i, todoTasks[i]);
         }
-        console.log("---------------");
+        console.log("----------");
     }
     else if(req === "add") {
-        let task = prompt("Please enter the task you want to add");
+        let task = prompt("Enter the task you want to add");
         todoTasks.push(task);
         console.log("Task Added");
     }
-
     else if(req === "delete") {
-        let idx = Number(prompt("Enter the task index you want to delete"));
-        todoTasks.splice(idx, 1);
+        let indx = Number(prompt("Enter the task index you want to delete"));
+        todoTasks.splice(indx, 1);
         console.log("Task Deleted");
-    }
-
-    else {
-        console.log("Wrong Request");
     }
 
     req = prompt("Enter your request");

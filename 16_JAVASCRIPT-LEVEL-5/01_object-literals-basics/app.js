f1a281e5ecaba6a879db439e086b4a731ed05220
adit5156 <adityasinghmpc2@gmail.com>
2024-09-11 11:39:42 +0530

    updated 11-09-2024

---
+++ 16_JAVASCRIPT-LEVEL-5/01_object-literals-basics/app.js
@@ -0,0 +1,35 @@
+let student = {
+    name: "Aditya",
+    age: 19,
+    marks: 94.4,
+    city: "Delhi"
+};
+
+student.city = "Mumbai";
+student.gender = "Male";
+student.marks = "A";
+
+const item = {
+    price: 100.99,
+    discount: 50,
+    colors: ["red", "pink"]
+};
+
+// Thread / Twitter Post
+
+let post = {
+    username: "@aditya5156",
+    content: "This is my #firstpost",
+    likes: 100,
+    reposts: 5,
+    tags: ["@muscleblaze", "@pintola", "@asitisnutrition"]
+}
+
+let obj = {
+    1: "a",
+    2: "b",
+    null: "d",
+    true: "c",
+    undefined: "e"
+};
+

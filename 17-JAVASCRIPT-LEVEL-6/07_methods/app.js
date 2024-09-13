90bc3d472df3c5e487596670d8b0e3b5a0bba59a
adit5156 <adityasinghmpc2@gmail.com>
2024-09-13 17:56:09 +0530

    updated 13-09-2024

---
+++ 17-JAVASCRIPT-LEVEL-6/07_methods/app.js
@@ -0,0 +1,27 @@
+// Actions that can be performed on the objects are the methods.
+
+// const calculator = {
+//     num: 55,
+//     add: function(a, b) {
+//         return a + b;
+//     },
+//     sub: function(a, b) {
+//         return a - b;
+//     },
+//     mul: function(a, b) {
+//         return a * b;
+//     }
+// };
+
+const calculator2 = {
+    add(a, b) {
+        return a + b;
+    },
+    sub(a, b) {
+        return a - b;
+    },
+    mul(a, b) {
+        return a * b;
+    }
+};
+

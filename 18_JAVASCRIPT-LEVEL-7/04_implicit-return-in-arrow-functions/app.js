98ef506120c83967523ea7dc8635e945bf691f2f
adit5156 <adityasinghmpc2@gmail.com>
2024-09-15 21:02:16 +0530

    updated 15-09-2024

---
+++ 18_JAVASCRIPT-LEVEL-7/04_implicit-return-in-arrow-functions/app.js
@@ -0,0 +1,12 @@
+// Implicit return in arrow functions
+
+// const mul = (a, b) => a * b;
+
+const sum = (a, b) => (a + b);
+
+const mul = (a, b) => (a * b);
+
+const square = (n) => (n ** 2);
+
+const cube = (n) => (n ** 3);
+

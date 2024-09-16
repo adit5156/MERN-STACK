62cc7eff637d69f446f775ef676146be728e634e
adit5156 <adityasinghmpc2@gmail.com>
2024-09-16 18:05:44 +0530

    updated 16-09-2024

---
+++ 18_JAVASCRIPT-LEVEL-7/08_this-with-arrow-function/app.js
@@ -0,0 +1,27 @@
+const student = {
+    name: "Aman",
+    marks: 95,
+    prop: this,
+
+    getName: function() {
+        console.log(this);
+        return this.name;
+    },
+
+    getMarks: () => {
+        console.log(this);
+        return this.marks;
+    },
+
+    getInfo1: function() {
+        setTimeout(() => {
+            console.log(this);
+        }, 2000);
+    },
+
+    getInfo2: function() {
+        setTimeout(function() {
+            console.log(this);
+        }, 2000)
+    }
+}

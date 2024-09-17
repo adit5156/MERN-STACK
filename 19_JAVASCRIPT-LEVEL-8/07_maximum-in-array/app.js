94d3b3d302da8a6765e76fdd6df320e77d05bb18
adit5156 <adityasinghmpc2@gmail.com>
2024-09-17 21:14:13 +0530

    updated 17-09-2024

--- 19_JAVASCRIPT-LEVEL-8/07_maximum-in-array/app.js
+++ 19_JAVASCRIPT-LEVEL-8/07_maximum-in-array/app.js
@@ -1,21 +1,22 @@
 let arr = [1, 4, 2, 5, 6, 7, 2, 9, 2];
+let max = 0;
 
-// let max = -1;
+for(let i = 0; i < arr.length; i++) {
+    if(max < arr[i]) {
+        max = arr[i];
+    }
+}
 
-// for(let i = 0; i < arr.length; i++) {
-//     if(max < arr[i]) {
-//         max = arr[i];
-//     }
-// };
+console.log("Maximum number in the array = ", max);
 
-// console.log(max);
 
-// let max = arr.reduce((acc, element) => {
-//     if(acc < element) {
-//         return element;
-//     }
-//     else {
-//         return acc;
-//     }
-// });
-// console.log(max);
+
+max = arr.reduce((acc, element) => {
+    if(acc < element) {
+        return element;
+    }
+    else {
+        return acc;
+    }
+});
+console.log("Maximum number in the array = ", max);

17c3cae17b3b45643b7554aec8baed495e7ef39a
adit5156 <adityasinghmpc2@gmail.com>
2024-09-17 18:53:34 +0530

    updated 17-09-2024

---
+++ 19_JAVASCRIPT-LEVEL-8/07_maximum-in-array/app.js
@@ -0,0 +1,21 @@
+let arr = [1, 4, 2, 5, 6, 7, 2, 9, 2];
+
+// let max = -1;
+
+// for(let i = 0; i < arr.length; i++) {
+//     if(max < arr[i]) {
+//         max = arr[i];
+//     }
+// };
+
+// console.log(max);
+
+// let max = arr.reduce((acc, element) => {
+//     if(acc < element) {
+//         return element;
+//     }
+//     else {
+//         return acc;
+//     }
+// });
+// console.log(max);

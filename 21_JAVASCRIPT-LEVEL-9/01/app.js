79e1e08cad6507f047c50556298a3b0f0fe97bf7
adit5156 <adityasinghmpc2@gmail.com>
2024-09-20 14:22:40 +0530

    updated 20-09-2024

---
+++ 21_JAVASCRIPT-LEVEL-9/01/app.js
@@ -0,0 +1,25 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let smallImage of smallImages) {
+//     smallImage.src = "assets/spiderman_img.png";
+//     console.log(smallImage.src);
+// }
+
+let query = document.querySelector('h1');
+console.log(query);
+console.log(query.textContent);
+
+query = document.querySelector('#description');
+console.log(query);
+
+query = document.querySelector('.oldImg');
+console.log(query);
+
+query = document.querySelector('.box a');
+console.log(query);
+
+let queryAll = document.querySelectorAll('p');
+console.log(queryAll);
+
+queryAll = document.querySelectorAll('.box a');
+console.log(queryAll);

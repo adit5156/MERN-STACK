27f42b29b11bde15ce0945148fffed426fc4a981
adit5156 <adityasinghmpc2@gmail.com>
2024-09-26 20:20:08 +0530

    updated 26-09-2024

---
+++ 23_JAVASCRIPT-MINI-PROJECT/03_simon-game-setup/app.js
@@ -0,0 +1,51 @@
+let h2 = document.querySelector("h2");
+
+let gameSeq = [];
+let userSeq = [];
+let colors = ["red", "yellow", "green", "purple"];
+
+let gameStarted = false;
+let level = 0;
+
+document.addEventListener("keypress", function() {
+    if(gameStarted == false) {
+        console.log("Game has been started");
+        gameStarted = true;
+
+        levelUp();
+    }
+});
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+    let randomNum = Math.floor(Math.random() * 4);
+    let randomColor = colors[randomNum];
+    let randomBtn = document.querySelector(`.${randomColor}`);
+    gameFlash(randomBtn);
+}
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    }, 200);
+}
+
+function userFlash(btn) {
+    btn.classList.add("userFlash");
+    setTimeout(function() {
+        btn.classList.remove("userFlash");
+    }, 200);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function btnPress() {
+    let btn = this;
+    console.log(btn);
+    userFlash(btn);
+}

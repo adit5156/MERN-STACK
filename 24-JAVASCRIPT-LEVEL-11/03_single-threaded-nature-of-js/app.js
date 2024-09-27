730bd1ca5763414484151c03758640538525ca16
adit5156 <adityasinghmpc2@gmail.com>
2024-09-27 21:11:44 +0530

    updated 27-09-2024

---
+++ 24-JAVASCRIPT-LEVEL-11/03_single-threaded-nature-of-js/app.js
@@ -0,0 +1,21 @@
+// Synchronous Code
+
+// let a = 25;
+// console.log(a);
+// let b = 10;
+// console.log(b);
+// console.log(a+b);
+
+
+
+// Asynchronous Code
+
+setTimeout(() => {
+    console.log("Apna College");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Hello, Apna College");
+}, 3000);
+
+console.log("Hello...");

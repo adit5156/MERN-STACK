ad07994b89c30e60221fc26f78b14f9f0d5ff210
adit5156 <adityasinghmpc2@gmail.com>
2024-09-30 01:28:55 +0530

    updated 30-09-2024

--- 24-JAVASCRIPT-LEVEL-11/practice.js
+++ 24-JAVASCRIPT-LEVEL-11/practice.js
@@ -84,100 +84,110 @@
 //     }
 // }
 
-// saveToDb("hello aditya", 
+// saveToDb("Apple India",
 //     (data) => {
-//         console.log("Sucess: data has been saved. ", data);
-//         saveToDb("welcome to apna college",
+//         console.log("Sucess1: Data is saved i.e.", data);
+//         saveToDb("HP India",
 //             (data) => {
-//                 console.log("Sucess2: data has been saved. ", data);
-//                 saveToDb("Sigma 4.0", 
+//                 console.log("Sucess2: Data is saved i.e.", data);
+//                 saveToDb("Lenovo India", 
 //                     (data) => {
-//                         console.log("Sucess3: data has been saved. ", data);
+//                         console.log("Success3: Data is saved i.e.", data);
+//                         saveToDb("Acer India", 
+//                             (data) => {
+//                                 console.log("Success4: Data is saved i.e.", data);
+//                             },
+//                             (data) => {
+//                                 console.log("Failure: Weak connection i.e.", data);
+//                             }
+//                         );
 //                     },
 //                     (data) => {
-//                         console.log("Failure: weak connection...data not saved. ", data);
+//                         console.log("Failure: Weak connection i.e.", data);
 //                     }
-//                 )
+//                 );
 //             },
 //             (data) => {
-//                 console.log("Failure: weak connection...data not saved. ", data);
+//                 console.log("Failure: Weak connection i.e.", data);
 //             }
-//         )
+//         );
 //     },
 //     (data) => {
-//         console.log("Failure: weak connection...data not saved. ", data);
+//         console.log("Failure: Weak connection i.e.", data);
 //     }
 // );
 
-// function saveToDb(data) {
-//     return new Promise((resolve, reject) => {
-//         let internetSpeed = Math.floor(Math.random() * 10) + 1;
-//         if(internetSpeed > 4) {
-//             resolve(data);
-//         }
-//         else {
-//             reject(data);
-//         }
-//     });
-// }
-
-// console.log(saveToDb("Aditya Singh"));
-
-// let request = saveToDb("Apple India");
-// request
-// .then((result) => {
-//     console.log("Data1 is saved i.e.", result);
-//     return saveToDb("HP India");
-// })
-// .then((result) => {
-//     console.log("Data2 is saved i.e.", result);
-//     return saveToDb("Lenovo India");
-// })
-// .then((result) => {
-//     console.log("Data3 is saved i.e.", result);
-//     return saveToDb("Dell India");
-// })
-// .then((result) => {
-//     console.log("Data4 is saved i.e.", result);
-// })
-// .catch((error) => {
-//     console.log("Weak connection. Data not saved i.e.", error);
-// });
+// Callback Nesting / Callback Hell
 
 
 
 
-let h1 = document.querySelector("h1");
-
-function colorChange(color, delay) {
+function saveToDb(data) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            h1.style.color = color;
-            resolve(color);
-        }, delay);
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if(internetSpeed > 4) {
+            resolve(data);
+        }
+        else {
+            reject(data);
+        }
     });
 }
 
-colorChange("red", 1000)
+saveToDb("Apple India")
 .then((result) => {
-    console.log(`Color has been changed to ${result}`);
-    return colorChange("green", 1000);
+    console.log("Success1: Data is saved i.e.", result);
+    return saveToDb("HP India");
 })
 .then((result) => {
-    console.log(`Color has been changed to ${result}`);
-    return colorChange("blue", 1000);
+    console.log("Success2: Data is saved i.e.", result);
+    return saveToDb("Lenovo India");
 })
 .then((result) => {
-    console.log(`Color has been changed to ${result}`);
-    return colorChange("orange", 1000);
+    console.log("Success3: Data is saved i.e.", result);
+    return saveToDb("Dell India");
 })
 .then((result) => {
-    console.log(`Color has been changed to ${result}`);
-    return colorChange("hotpink", 1000);
-})
-.then((result) => {
-    console.log(`Color has been changed to ${result}`);
+    console.log("Success4: Data is saved i.e.", result);
 })
 .catch((error) => {
-    console.log("Promise was rejected");
+    console.log("Failure: Weak Connection i.e.", error);
 });
+
+
+
+
+// let h1 = document.querySelector("h1");
+
+// function colorChange(color, delay) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             h1.style.color = color;
+//             resolve(color);
+//         }, delay);
+//     });
+// }
+
+// colorChange("red", 1000)
+// .then((result) => {
+//     console.log(`Color has been changed to ${result}`);
+//     return colorChange("green", 1000);
+// })
+// .then((result) => {
+//     console.log(`Color has been changed to ${result}`);
+//     return colorChange("blue", 1000);
+// })
+// .then((result) => {
+//     console.log(`Color has been changed to ${result}`);
+//     return colorChange("orange", 1000);
+// })
+// .then((result) => {
+//     console.log(`Color has been changed to ${result}`);
+//     return colorChange("hotpink", 1000);
+// })
+// .then((result) => {
+//     console.log(`Color has been changed to ${result}`);
+// })
+// .catch((error) => {
+//     console.log("Promise was rejected");
+// });

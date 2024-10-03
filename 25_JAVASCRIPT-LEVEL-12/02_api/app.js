d430149bc62893cf3bf3120a9fe927ef968b5d8c
adit5156 <adityasinghmpc2@gmail.com>
2024-10-03 06:37:37 +0530

    updated 03-10-2024

--- 25_JAVASCRIPT-LEVEL-12/02_api/app.js
+++
@@ -1,4 +0,0 @@
-let jsonData = '{"fact":"Among many other diseases, cats can suffer from anorexia, senility, feline AIDS and acne.","length":89}';
-
-console.log(jsonData);
-console.log(JSON.parse(jsonData));

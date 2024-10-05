714ffbee09baf4e9bb9300fe69299a1a74712d4a
adit5156 <adityasinghmpc2@gmail.com>
2024-10-05 21:53:06 +0530

    updated 05-10-2024

---
+++ 27_JAVASCRIPT-LEVEL-11-(REVISION)/01_callstack.js
@@ -0,0 +1,25 @@
+// function one() {
+//     return 1;
+// }
+
+// function two() {
+//     return one() + one();
+// }
+
+// function three() {
+//     let sum = two() + one()
+//     console.log(sum);
+// }
+
+// three();
+
+
+setTimeout(() => {
+    console.log("Apna College");
+}, 2000);
+
+setTimeout(() => {
+    console.log("All the very best!");
+}, 2000);
+
+console.log("Hello from, ");

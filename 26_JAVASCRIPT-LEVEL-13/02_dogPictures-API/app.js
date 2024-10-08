f36c81e06564433f0b46d75e71539e4262386025
adit5156 <adityasinghmpc2@gmail.com>
2024-10-08 10:19:47 +0530

    updated 08-10-2024

--- 26_JAVASCRIPT-LEVEL-13/02_dogPictures-API/app.js
+++ 26_JAVASCRIPT-LEVEL-13/02_dogPictures-API/app.js
@@ -1,24 +1,20 @@
 let url = "https://dog.ceo/api/breeds/image/random";
 
-let h1 = document.querySelector("h1");
 let img = document.querySelector("img");
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", async () => {
-    let dogImg = await getImg();
-    img.setAttribute('src', dogImg);
-});
+    let link = await getDogImages();
+    // console.log(link);
+    img.src = link;
+})
 
-async function getImg() {
+async function getDogImages() {
     try {
         let request = await axios.get(url);
-        console.log(request);
-        console.log(request.data);
-        console.log(request.data.message);
-        return (request.data.message);
+        return request.data.message;
     }
     catch(error) {
-        console.log("Error occured:", error);
-        return "No image found!";
+        return `No Dog Images Found`;
     }
 }

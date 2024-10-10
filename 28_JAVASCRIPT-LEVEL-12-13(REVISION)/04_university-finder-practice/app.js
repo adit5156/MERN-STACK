fd01edf7c9075dd5d91607a8107cb2acad02c18d
adit5156 <adityasinghmpc2@gmail.com>
2024-10-10 17:53:11 +0530

    updated 10-10-2024

---
+++ 28_JAVASCRIPT-LEVEL-12-13(REVISION)/04_university-finder-practice/app.js
@@ -0,0 +1,26 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ol = document.querySelector("ol");
+
+btn.addEventListener("click", async () => {
+    let url = `http://universities.hipolabs.com/search?name=${inp.value}`;
+    let responses = await getUni(url);
+    inp.value = "";
+    ol.innerText = "";
+    for(let response of responses) {
+        let uniName = response.name;
+        let uniCountry = response.country;
+        createElement(uniName, uniCountry);
+    }
+});
+
+function createElement(uniName, uniCountry) {
+    let li = document.createElement("li");
+    li.innerText = `${uniName}, ${uniCountry}`;
+    ol.appendChild(li);
+}
+
+async function getUni(url) {
+    let request = await axios.get(url);
+    return request.data;
+}

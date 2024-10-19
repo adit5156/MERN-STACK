8d0d7c042a08dbab4b676fa9fb1304edc09a755a
adit5156 <adityasinghmpc2@gmail.com>
2024-10-19 17:29:56 +0530

    revised api calls. updated 19-10-2024

--- 28_JAVASCRIPT-LEVEL-12-13(REVISION)/05_university-finder-practice-2/app.js
+++ 28_JAVASCRIPT-LEVEL-12-13(REVISION)/05_university-finder-practice-2/app.js
@@ -2,19 +2,19 @@ let inp = document.querySelector("input");
 let btn = document.querySelector("button");
 let ol = document.querySelector("ol");
 
-btn.addEventListener("click", async () => {
+btn.addEventListener("click", async function() {
     let url = `http://universities.hipolabs.com/search?name=${inp.value}`;
-    let responses = await getUni(url);
+    let universities = await getUni(url);
     inp.value = "";
     ol.innerText = "";
-    for(let response of responses) {
-        let uniName = response.name;
-        let uniCountry = response.country;
-        createElement(uniName, uniCountry);
+    for(let university of universities) {
+        let uniName = university.name;
+        let uniCountry = university.country;
+        await createElement(uniName, uniCountry);
     }
 });
 
-function createElement(uniName, uniCountry) {
+async function createElement(uniName, uniCountry) {
     let li = document.createElement("li");
     li.innerText = `${uniName}, ${uniCountry}`;
     ol.appendChild(li);

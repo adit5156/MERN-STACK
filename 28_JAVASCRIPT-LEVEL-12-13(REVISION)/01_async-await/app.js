9056dde9567df13e7c739c1a5f3d39084eaa1446
adit5156 <adityasinghmpc2@gmail.com>
2024-10-26 19:13:11 +0530

    revision. 26-10-2024

--- 28_JAVASCRIPT-LEVEL-12-13(REVISION)/01_async-await/app.js
+++ 28_JAVASCRIPT-LEVEL-12-13(REVISION)/01_async-await/app.js
@@ -34,43 +34,39 @@
 // demo();
 
 
-// let h1 = document.querySelector("h1");
+let h1 = document.querySelector("h1");
 
-// function colorChange(color, delay) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             h1.style.color = color;
-//             console.log(`Color has been changed to ${color}`);
-//             resolve();
-//         }, delay);
-//     });
-// }
+function colorChange(color,delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve(console.log(`Color has been changed to ${color}`));
+        }, delay);
+    });
+}
 
-// async function testColorChange() {
-//     try {
-//         await colorChange("red", 1000);
-//         await colorChange("green", 1000);
-//         await colorChange("blue", 1000);
-//         await colorChange("orange", 1000);
-//         await colorChange("hotpink", 1000);
-//         await colorChange("yellow", 1000);
-//         await colorChange("aqua", 1000);
-//     }
-//     catch(error) {
-//         console.log(`Color has not been changed to ${error}`);
-//     }
-// }
+async function testColorChange() {
+    try {
+        await colorChange("red", 1000);
+        await colorChange("green", 1000);
+        await colorChange("blue", 1000);
+        await colorChange("orange", 1000);
+        await colorChange("hotpink", 1000);
+        await colorChange("aqua", 1000);
+    }
+    catch(error) {
+        console.log(`No Color Found ${error}`);
+    }
+}
 
-// testColorChange();
-
+testColorChange();
 
 
 // function saveToDb(data) {
 //     return new Promise((resolve, reject) => {
 //         let internetSpeed = Math.floor(Math.random() * 10) + 1;
 //         if(internetSpeed > 4) {
-//             console.log(`Success: Data is saved i.e. ${data}`);
-//             resolve();
+//             resolve(console.log(`Success: Data is saved i.e. ${data}`));
 //         }
 //         else {
 //             reject(data);
